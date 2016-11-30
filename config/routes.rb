863d02fd46cafe300a1d768057652c7442a6bf56
Robert <obviouslyrob@gmail.com>
2016-11-30 22:11:26 +0000

    Connect navbar dropdown to other static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Betrhgm2016::Application.routes.draw do
   root 'static_pages#home'
-  get "static_pages/about"
-  get "static_pages/contact"
+  
+  get    '/help',     to: 'static_pages#help'
+  get    '/about',    to: 'static_pages#about'
+  get    '/contact',  to: 'static_pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
